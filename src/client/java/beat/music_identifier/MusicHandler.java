aeb1d511c07bb82fa5be850888bb62dedd4bd9f3
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-05-28 18:27:48 -0700

    🐛 Fix Main Menu Bug

--- src/client/java/beat/music_identifier/MusicHandler.java
+++ src/client/java/beat/music_identifier/MusicHandler.java
@@ -9,13 +9,17 @@ import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 
 public class MusicHandler implements SoundInstanceListener {
     @Override
     public void onSoundPlayed(SoundInstance sound, WeightedSoundSet soundSet) {
         MusicIdentifierConfig config = AutoConfig.getConfigHolder(MusicIdentifierConfig.class).getConfig();
+        World world = MinecraftClient.getInstance().world;
 
-        if (sound.getCategory() == SoundCategory.MUSIC) {
+        // world != null make sure that the user is in a world
+        // and not the main menu
+        if (sound.getCategory() == SoundCategory.MUSIC && world != null) {
             Text name = Util.getSoundName(sound);
             if (name == null) return;
 
